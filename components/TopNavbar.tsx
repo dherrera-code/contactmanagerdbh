0044c04936466a0f6bc37be012f88ab4a1acad94
dherrera-code <dherr929@gmail.com>
2026-03-17 11:20:05 -0700

    created topnavbar component, need functionality!

---
+++ components/TopNavbar.tsx
@@ -0,0 +1,31 @@
+import { Navbar, NavbarBrand, NavbarToggle, NavbarCollapse, NavbarLink, TextInput } from 'flowbite-react'
+import Link from 'next/link'
+import React from 'react'
+
+const TopNavbar = () => {
+    return (
+        <Navbar fluid className='dark:bg-white bg-white border-b-mist-200 border-b-2'>
+            <NavbarBrand className='ps-2' >
+                <img src="/assets/ContactFlow-logo.png" className="mr-3 h-9 sm:h-11" alt="Flowbite React Logo" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-black text-black">Contact Manager</span>
+            </NavbarBrand>
+            <NavbarToggle />
+            <NavbarCollapse>
+                {/* <TextInput className='' size={33} placeholder='Search contacts...'></TextInput> */}
+                <form className="max-w-md mx-auto">
+                    <label className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div>
+                        <input type="search" id="default-search" className="block w-80 h-10 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search contacts..." required />
+                    </div>
+                </form>
+            </NavbarCollapse>
+        </Navbar>
+    )
+}
+
+export default TopNavbar
